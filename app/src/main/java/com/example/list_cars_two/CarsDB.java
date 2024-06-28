92c62f142fbe425755e3647793ba004e0ff7308f
sistemascortina <sistemas@cortinaconsultores.com>
2024-06-28 17:07:52 -0600

    car list

--- app/src/main/java/com/example/list_cars_two/CarsDB.java
+++ app/src/main/java/com/example/list_cars_two/CarsDB.java
@@ -8,14 +8,42 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class CarsDB extends SQLiteOpenHelper {
-    public CarsDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version){
-        super(context,name, factory,version);
+    public CarsDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
     }
+
     @Override
-    public void onCreate(SQLiteDatabase db){
+    public void onCreate(SQLiteDatabase db) {
         db.execSQL(CarsConstant.createTable);
         System.out.println("DATABASE CREEATED.");
     }
 
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    @Override
+    public long insertData(String carName, String carPrice, String carBrand, String carMotor, String carYear, String carKilometer, String carGuaranty) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues cV = new ContentValues();
+
+        cV.put("name", carName);
+        cV.put("price", carPrice);
+        cV.put("year", carYear);
+        cV.put("kilometer", carKilometer);
+        cV.put("motor", carMotor);
+        cV.put("guaranty", carGuaranty);
+        cV.put("brand", carBrand);
+
+        long result = db.insert(CarsConstant.carsTable, null, cV);
+
+    }
+
+    public Cursor rawQuery(String sql, String [] parameters){
+        SQLiteDatabase connect = this.getWritableDatabase();
+        return connect.rawQuery(sql,parameters);
+    }
 
 }
